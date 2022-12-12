7cb13212fc3eea1a3119e2d46bb83f70ba6f960c
Marthe-M <martheminderman@gmail.com>
2022-12-12 12:23:44 +0100

    added login controller

--- Models/UserLogin.cs
+++ Models/UserLogin.cs
@@ -1,12 +1,10 @@
 ﻿namespace BookRental_dotnet.Models
 {
-    public class UserRegistration
+    public class UserLogin
     {
-         public string email { get; set; } = String.Empty;
         public string username { get; set; } = String.Empty;
         public string password { get; set; } = String.Empty;
         public bool isAdmin { get; set; } = false; 
-        public bool firstTimeLogin { get; set; } = true; 
-
+  
      }
 }
