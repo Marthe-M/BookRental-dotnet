f094a00d1d680fdb992e06fd33f47e69b688024e
Marthe-M <martheminderman@gmail.com>
2023-01-05 15:00:09 +0100

    remove files

--- Models/AddReservationRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookRental_dotnet.Models
-{
-    public class AddReservationRequest
-    {
-        public bool approved { get; set; }
-        public Guid userId { get; set; }
-        public Guid bookId { get; set; }
-    }
-}
